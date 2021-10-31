052db8c09a8874b9eeb45980b9460d9fa6a5a665
Eduard Loktev <lwqtbdwrd@gmail.com>
2021-10-31 12:14:19 +0200

    add:new JS fetures

--- scripts/script.js
+++ scripts/script.js
@@ -9,19 +9,55 @@ const popupMessageBtn=document.querySelector('.popup__submit-button_type_message
 const closeJoinPopupBtn=document.querySelector('.popup__close-button');
 const closeMessageBtn=document.querySelector('.popup__close-button_type_message');
 const closeContactPopupBtn=document.querySelector(".popup__close-button_type_contact");
+const openMenuBtn=document.querySelector('.menu__button');
+const menuPopup=document.querySelector('.menu');
+const menuLinks=document.querySelectorAll('.menu__link');
+const menuLogo=document.querySelector('.menu__logo');
 function openPopup(popup){
     popup.classList.add(`popup_is-opened`);
 }
 function closePopup(popup){
   popup.classList.remove(`popup_is-opened`);
+  [...document.querySelectorAll(".popup__form")].forEach((form)=>form.reset());
 }
-joinInLink.addEventListener('click', ()=>{openPopup(popupFormJoin)});
-partnersButton.addEventListener('click',()=> {openPopup(popupFormJoin)});
 
-contactLink.addEventListener('click',()=>{ openPopup(contactPopup)});
+joinInLink.addEventListener('click',()=> {openPopup(popupFormJoin); closePopup(contactPopup)} );
+contactLink.addEventListener('click',()=> {openPopup(contactPopup);closePopup(popupFormJoin) });
+contactPopup.addEventListener('submit',(evt)=>{evt.preventDefault(); openPopup(messagePopup);});
 
-contactPopup.addEventListener('submit' , (evt)=> {evt.preventDefault(); openPopup(messagePopup)});
-closeJoinPopupBtn.addEventListener('click', ()=> closePopup(popupFormJoin));
-contactPopup.addEventListener('submit', ()=>{closePopup(messagePopup)} );
-closeContactPopupBtn.addEventListener("click", ()=> {closePopup(contactPopup)});
 
+const allPopups= [...document.querySelectorAll('.popup')];
+allPopups.forEach((popup)=>popup.addEventListener("click", (evt)=>{
+  if (evt.target.classList.contains('popup_is-opened')){
+    console.log(evt.target);
+
+  closePopup(evt.target);
+  }
+  
+  }));
+openMenuBtn.addEventListener('click', (evt)=>{
+  if (evt.target.classList.contains(`menu__is-open`)){
+    closePopup(menuPopup);
+    openMenuBtn.classList.remove(`menu__is-open`);
+  }
+  else{
+    openMenuBtn.classList.add(`menu__is-open`);
+    openPopup(menuPopup); 
+    closePopup(contactPopup);
+    closePopup(popupFormJoin);
+  }
+} );
+[...menuLinks].forEach((link)=>link.addEventListener('click', ()=>{closePopup(menuPopup); openMenuBtn.classList.remove(`menu__is-open`);}));
+let prevScrollpos = window.pageYOffset;
+document.addEventListener('scroll',()=>{
+ let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    menuPopup.classList.remove(`menu__hidden`);
+    menuLogo.classList.remove(`menu__hidden`);
+  } else {
+    menuPopup.classList.add(`menu__hidden`);
+    menuLogo.classList.add(`menu__hidden`);
+  }
+  prevScrollpos = currentScrollPos;
+}
+);
